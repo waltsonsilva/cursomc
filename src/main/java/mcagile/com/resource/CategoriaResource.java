3fa01f0e9b719ec9154fab729a59b13d708f57be
waltson <waltson.silva@hotmail.com>
2018-05-07 15:01:16 -0300

    Testantdo primeira classe de dominio CATEGORIA

--- src/main/java/mcagile/com/resource/CategoriaResource.java
+++ src/main/java/mcagile/com/resource/CategoriaResource.java
@@ -1,16 +1,27 @@
 package mcagile.com.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import mcagile.com.domain.Categoria;
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return"REST está funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 
