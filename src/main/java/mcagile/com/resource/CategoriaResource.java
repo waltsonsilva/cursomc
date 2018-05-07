0d541f402e439cf377b350dcb1ee5e8468d14908
waltson <waltson.silva@hotmail.com>
2018-05-07 14:41:03 -0300

    Testantdo REST

---
+++ src/main/java/mcagile/com/resource/CategoriaResource.java
@@ -0,0 +1,17 @@
+package mcagile.com.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return"REST está funcionando";
+	}
+	
+
+}
